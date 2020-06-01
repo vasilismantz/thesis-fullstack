749c471199bedd2299426f185b9c02904ae9b3c7
VasilisMantz <mantzaris.vas98@gmail.com>
2020-06-01 13:36:28 +0300

    Completed Calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -3,58 +3,94 @@ import React, { Component } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-
-import AddEventModal from './AddEventPopup'
+import axios from "axios";
+import AddEventPopup from "./AddEventPopup";
+import moment from "moment";
+import ReactToolTip from "react-tooltip";
 
 export default class Calendar extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            events: [],
-            showModel: false
-        };
-    }
-
-    onClick = () => {
-        var joined = this.state.events.concat({
-            title: "Test",
-            start: "2020-04-29",
-            end: "2020-04-30"
-        });
-        this.setState({ events: joined });
-        console.log(this.state);
+    this.state = {
+      user: {},
+      events: [],
+      showModel: false,
     };
+  }
 
-    handleEventClick = (arg) => {
-        this.removeEvent(arg.event)
-    }
+  componentDidMount() {
+    this.setState({ user: JSON.parse(localStorage.getItem("user")) }, () => {
+      axios({
+        method: "get",
+        url: `api/personalEvents/user/${this.state.user.id}`,
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then((res) => {
 
-    removeEvent = (event) => {
-        var newEvents = [...this.state.events]; // make a separate copy of the array
-        newEvents.splice(event, 1);
-        this.setState({ events: newEvents });
-    }
+        let newEvents = res.data.map((x) => ({
+        title: x.eventTitle,
+          description: x.eventDescription,
+          start: x.eventStartDate,
+          end: x.eventEndDate,
+          id: x.id,
+          color: '#007bff'
 
-    render() {
+        }));
 
-        let closeModel = () => this.setState({showModel: false})
+        for (var i in newEvents) {
+          newEvents[i].start = moment
+            (newEvents[i].start)
+            .format("YYYY-MM-DD HH:mm:ss");
+          newEvents[i].end = moment
+            (newEvents[i].end)
+            .format("YYYY-MM-DD HH:mm:ss");
+        }
 
-        return (
-            <>
-                <FullCalendar
-                    defaultView="dayGridMonth"
-                    plugins={[dayGridPlugin, interactionPlugin]}
-                    eventClick={this.handleEventClick}
-                    dateClick={() => this.setState({showModel: true})}
-                    events={this.state.events}
-                />
-                <AddEventModal
-                    show={this.state.showModel}
-                    onHide={closeModel}
-                />
-                <button onClick={this.onClick}>Test</button>
-            </>
-        );
-    }
+        this.setState({ events: [...newEvents] });
+      });
+    });
+  }
+
+  handleEventPositioned(info) {
+    info.el.setAttribute("title", info.event.extendedProps.description ? info.event.extendedProps.description : 'No description');
+    ReactToolTip.rebuild();
+  }
+
+  removeEvent = (event) => {
+    var newEvents = [...this.state.events]; // make a separate copy of the array
+    newEvents.splice(event, 1);
+    this.setState({ events: newEvents });
+  };
+
+  render() {
+    let closeModel = () => this.setState({ showModel: false });
+
+    return (
+      <>
+        <FullCalendar
+          defaultView="dayGridMonth"
+          plugins={[dayGridPlugin, interactionPlugin]}
+          eventClick={this.handleEventClick}
+          dateClick={() => this.setState({ showModel: true })}
+          events={this.state.events}
+          eventPositioned={this.handleEventPositioned}
+          customButtons={{
+            button: {
+              text: "Add Event",
+              click: () => {
+                this.setState({showModel: true})
+              },
+            },
+          }}
+          header={{
+            left: "prev,next today button",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+          }}
+        />
+        <AddEventPopup show={this.state.showModel} onHide={closeModel} />
+      </>
+    );
+  }
 }
