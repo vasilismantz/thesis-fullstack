c711a5bc3165e5c2da34d163861698099fc4123b
VasilisMantz <mantzaris.vas98@gmail.com>
2020-06-05 16:18:54 +0300

    Added JobOld
    Fixed Association

--- server/models/index.js
+++ server/models/index.js
@@ -29,11 +29,13 @@ db.department = require("./department.model")(sequelize, Sequelize);
 db.deptAnnouncement = require("./deptAnnouncement.model")(sequelize, Sequelize);
 db.organization = require("./organization.model")(sequelize, Sequelize);
 db.job = require("./job.model")(sequelize, Sequelize);
+db.jobOld = require('./jobOld.model')(sequelize, Sequelize)
 db.application = require("./application.model")(sequelize, Sequelize);
 db.daysWorking = require("./daysWorking.model")(sequelize, Sequelize);
 db.daysHoliday = require("./daysHoliday.model")(sequelize, Sequelize);
 db.payment = require("./payment.model")(sequelize, Sequelize);
 db.expense = require("./expense.model")(sequelize, Sequelize);
+db.images = require("./image.model")(sequelize, Sequelize)
 
 
 // Organization Associations
@@ -48,7 +50,9 @@ db.user.hasMany(db.userPersonalEvent, {foreignKey: {allowNull: false}})
 db.user.hasMany(db.userMessage, {foreignKey: {name:'receiver', allowNull: false}})
 db.user.hasMany(db.application, {foreignKey: {allowNull: false}})
 db.user.hasMany(db.deptAnnouncement, {foreignKey: {name: 'createdByUserId', allowNull: false}})
-db.user.hasMany(db.job, {foreignKey: {allowNull: true}})
+db.user.hasOne(db.job, {foreignKey: {allowNull: false}})
+db.user.hasMany(db.jobOld, {foreignKey: {allowNull: false}})
+db.user.belongsTo(db.department, {foreginKey: {allowNull: true}})
 
 // Department Associations
 db.department.hasMany(db.user)
@@ -59,7 +63,8 @@ db.department.hasMany(db.expense, {foreignKey: {allowNull: false}})
 db.userMessage.belongsTo(db.user, {foreignKey: {name:'sender', allowNull: false}})
 
 // Job Associations
-db.job.hasMany(db.payment, {foreginKey: {allowNull: false}})
+db.job.hasMany(db.payment, {foreginKey: {allowNull: true}})
+db.jobOld.hasMany(db.payment, {foreignKey: {allowNull: true}})
 
 // Application Associations
 db.application.belongsTo(db.user)
