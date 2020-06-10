c94fa10fcc0fee3d48f59c9ba267f17efdff925f
VasilisMantz <mantzaris.vas98@gmail.com>
2020-06-10 16:21:36 +0300

    Shows Alert when deleting a Department with Employees

---
+++ client/src/components/AlertModal.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Redirect } from 'react-router-dom'
+import { Modal, Alert, Button } from "react-bootstrap";
+
+
+export default class AlertModal extends Component {
+
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Warning
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            This Department has Employees. Transfer Employees to a new Department first.
+            <br />
+            <a href="/employee-list">Employee List</a>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
