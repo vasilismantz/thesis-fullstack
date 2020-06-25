db9f47a42504fac962e53317244491a3a0e96f35
VasilisMantz <mantzaris.vas98@gmail.com>
2020-06-26 01:30:27 +0300

    Fix

--- server/routes/userFinacnialInformation.routes.js
+++ server/routes/userFinacnialInformation.routes.js
@@ -8,8 +8,11 @@ const financialInformation = require("../controllers/userFinancialInformation.co
 // Create a new User Financial Information
 router.post('/', withAuth.verifyToken, withAuth.withRoleAdmin, financialInformation.create);
 
+// Retrieve all User Financial Information
+router.get('/', withAuth.verifyToken, withAuth.withRoleAdminOrManager, financialInformation.findAll)
+
 // Retrieve User Financial Information by User Id
-router.get('/user/:id', withAuth.verifyToken, financialInformation.findAllByUserId);
+router.get('/user/:id', withAuth.verifyToken, financialInformation.findByUserId);
 
 // Retrieve a single User Financial Information with an id
 router.get('/:id', withAuth.verifyToken, financialInformation.findOne);
