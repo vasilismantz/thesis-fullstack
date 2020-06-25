c1f40f56d7174b9fd29ffc28693fcbcec5b7611c
VasilisMantz <mantzaris.vas98@gmail.com>
2020-06-26 01:41:49 +0300

    Completed Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 import "../App.css";
 import Infobox from "./infobox";
 import Calendar from "./Calendar";
-import ChartsPage from "./ChartsPage";
+import ExpenseChartsPage from "./ExpenseChartsPage"
+import PaymentChartsPage from "./PaymentChartsPage"
 import RecentApplciations from "./RecentApplications"
 import axios from 'axios'
 
@@ -14,6 +15,7 @@ export default class Dashboard extends Component {
     this.state = {
       totalEmployees: 0,
       totalExpenses: 0,
+      totalPayments: 0,
       recentApplications: []
     }
   }
@@ -46,6 +48,21 @@ export default class Dashboard extends Component {
       } else {
       }
     })
+
+    //Fetch Payments Total
+    axios({
+      method: 'get',
+      url: 'api/payments/year/2020',
+      headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+    })
+    .then(res => {
+      let array = res.data
+      if(array.length>0){
+        let sum = array.reduce((a, b) => ({expenses: parseInt(a.expenses) + parseInt(b.expenses)}))
+        this.setState({totalPayments: sum.expenses})
+      } else {
+      }
+    })
   }
   render() {    
     return (
@@ -66,13 +83,18 @@ export default class Dashboard extends Component {
             <Infobox
               title="Total Expenses"
               description={this.state.totalExpenses + "€"}
-              color="bg-danger"
+              color="bg-warning"
               icon="fa fa-shopping-cart"
             />
           </div>
           {/* Third info-box */}
           <div className="col-md-4 col-sm-6 col-xs-12">
-            <Infobox title="This is a title" />
+            <Infobox
+              title="Total Payments"
+              description={this.state.totalPayments + "€"}
+              color="bg-danger"
+              icon="fa fa-money-check"
+            />
           </div>
         </div>
         {/* Second Row with Calendar and Expense Report */}
@@ -80,6 +102,12 @@ export default class Dashboard extends Component {
           {/* Calendar */}
           <div className="col-sm-6">
             <Calendar />
+            <div className="panel panel-default">
+              <div className="panel-heading with-border" style={{ "backgroundColor": "#515e73", color: "white" }}>
+                <h3 className="panel-title">Recent Applications</h3>
+              </div>
+              <RecentApplciations />
+            </div>
           </div>
           {/* Expense Report & Recent Applications */}
           <div className="col-md-6">
@@ -87,13 +115,13 @@ export default class Dashboard extends Component {
               <div className="panel-heading with-border" style={{ "backgroundColor": "#515e73", color: "white" }}>
                 <h3 className="panel-title">Expense Report</h3>
               </div>
-              <ChartsPage />
+              <ExpenseChartsPage />
             </div>
             <div className="panel panel-default">
               <div className="panel-heading with-border" style={{ "backgroundColor": "#515e73", color: "white" }}>
-                <h3 className="panel-title">Recent Applications</h3>
+                <h3 className="panel-title">Payment Report</h3>
               </div>
-              <RecentApplciations />
+              <PaymentChartsPage />
             </div>
           </div>
         </div>
