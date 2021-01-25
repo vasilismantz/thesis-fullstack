7861139e01c273ec81e002b10a57fff8cee44481
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-01-25 01:26:55 +0100

    :wrench: Add missing serve static files function

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var withAuth = require('../withAuth')
+var withAuth = require("../withAuth");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.status(404).send({message: "Navigate to /api for documentation"})
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
+  );
+}
 
-router.get('/checkToken', withAuth.checkToken)
+router.get("/checkToken", withAuth.checkToken);
 
 module.exports = router;
