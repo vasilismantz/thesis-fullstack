758c322e5c0815f157006a9e33aa40a6e47b8d12
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-01-25 01:33:11 +0100

    :wrench: Add correct path

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ var withAuth = require("../withAuth");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
