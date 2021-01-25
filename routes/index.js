331581c650de446b8f10b4c73b1413273b68427b
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-01-25 02:36:56 +0100

    :wrench: Fix express path

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,16 @@ var app = express();
 
 var withAuth = require("../withAuth");
 
+app.use(express.static("client/build"));
+router.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" }); //res.render('index', { title: 'Express' });
-  res.status(404).send({ message: "Navigate to /api for documentation" });
-});
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" }); //res.render('index', { title: 'Express' });
+//   res.status(404).send({ message: "Navigate to /api for documentation" });
+// });
 
 router.get("/checkToken", withAuth.checkToken);
 
